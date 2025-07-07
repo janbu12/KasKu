ab1807a870bc39a241ddfbbc56e323bdf3926b15
janbu12 <mizanmujadid2436@gmail.com>
2025-07-07 17:38:05 +0700

    refactor: remove realtime database firebase

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -5,7 +5,6 @@ const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_PATH)
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_REALTIME_DATABASE_URL
 })
 
 const firebaseConfig = {
